e5b55d871dac4c2a1927a5390fa214066ca8b22b
RodrigoAroeira <raroeirabr@gmail.com>
2026-07-31 13:17:31 -0300

    fix: remove extra character from alias
    Changed from `threedd` to `threed`

--- src/cli.rs
+++ src/cli.rs
@@ -14,7 +14,7 @@ pub struct Args {
 pub enum RendererType {
     #[value(name = "2D", alias = "twod")]
     TwoD,
-    #[value(name = "3D", alias = "threedd")]
+    #[value(name = "3D", alias = "threed")]
     ThreeD,
 }
 
